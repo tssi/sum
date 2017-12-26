a20decd22d3e896b96b5390f0b5c78e6ce03bf8f
RicoJoseTSSi <RicoJose.TSSi@gmail.com>
2017-12-26 19:39:46 +0800

    active modules

--- controllers/groups_controller.js
+++ controllers/groups_controller.js
@@ -2,22 +2,80 @@ define(['app','api'],function(app){
 	app.register.controller('GroupsController',['$scope','api','$uibModal',function($scope,api,$uibModal){
 		function LoadGroups(){
 			var successLoad = function(response){
-				console.log(response);
 				$scope.Groups = response.data;
 			};
 			var errorLoad = function(response){
 			};
 			api.GET('groups',successLoad,errorLoad);
 		};
+		function LoadModules(){
+			var successLoad = function(response){
+				$scope.Modules = response.data;
+			};
+			var errorLoad = function(response){
+			};
+			api.GET('modules',successLoad,errorLoad);
+		};
+		function LoadActiveModules(){
+			$scope.activeModules = [];
+			var am = $scope.activeGroup.modules;
+			for(var j in $scope.Modules){
+				var m = $scope.Modules[j];
+				if(am.indexOf(m.id) != -1){
+					$scope.activeModules.push(m);
+				}
+			}
+		};
 		$scope.init = function(){
 			$scope.Groups = [];
+			$scope.Modules = [];
 			LoadGroups();
+			LoadModules();
 		};
 		$scope.SetActiveGroup = function(group){
 			$scope.activeGroup = group;
+			LoadActiveModules();
 		};
 		$scope.removeGroupInfo = function(){
 			$scope.activeGroup = null;
+		};
+		$scope.OpenModal = function(){
+			var group;
+			var config = {
+				templateUrl:"ModalContent.html",
+				controller:"ModalController",
+				resolve:{
+					Group:function(){
+						return group;
+					}
+				}
+			};
+			var modal = $uibModal.open(config);
+			var promise = modal.result;
+			var callback = function(data){
+								$scope.Message = 'Modal closed';
+								LoadUsers();
+							};
+			var fallback = function(data){
+								$scope.Message = 'Modal dismissed';
+								//console.log($scope.activeGroup.modules);
+								//console.log($scope.Modules);
+							};
+			promise.then(callback,fallback);
+		};
+	}]);
+	app.register.controller('ModalController',['$scope','$uibModalInstance','api',function($scope,$uibModalInstance,api){
+		$scope.closeModal = function(){
+			$uibModalInstance.dismiss();
+		};
+		$scope.confirmModal = function(){
+			var success = function(response){
+				
+			};
+			var error = function(response){
+				
+			};
+			api.POST('groups',success,error);
 		};
 	}]);
 });
